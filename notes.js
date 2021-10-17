0c364b77292c0802aa5474efc720b8ed4d3e2c3a
Sparsh <68753589+i-sparsh-sharma@users.noreply.github.com>
2021-10-17 05:32:18 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,71 @@
+/*
+X----------SERVER----------X
+>> removed env code
+- GOOGLE_CLIENT_ID=646243628401-700htb5sc2sh0dhpdfvkphhi6kvhe6qb.apps.googleusercontent.com
+- GOOGLE_CLIENT_SECRET=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
+- AWS_S3_SECRET_KEY="+Kxvi3vtwjUuOPS3juBwJ6gXOLpp+dqgZPp4ADsm"
+- AWS_S3_ACCESS_KEY="AKIAS7F6DF4DX7R6ZQEX"
+
+>> npm i nodemon -D
+- helps develop node. js based applications 
+  by automatically restarting the node application 
+  when file changes in the directory are detected
+
+>> npm install @babel/preset-env @babel/cli @babel/node --save-dev
+- Babel installation cmd. Babel helps to convert
+  high level js (ES6) code to low level js (ES5).
+
+>> npm i mongoose cors helmet
+- helmet and cors are dependencies which are used to
+  make are project cross origin. Align with API calls
+  import data, so it is expernal package. It will be useful
+  at the time of deployement..
+
+>> Use of Router
+- to prevent reloading and render required components..
+
+>> bcrypt npm package
+- A library to help you hash passwords.
+
+>> npm install jsonwebtoken
+
+>> npm i passport-jwt
+- used for all authentication
+
+>> npm install passport@0.4.1
+- google api started working after
+  installing this version of passport
+
+>> mongoDm Login
+- msunnyhihu@gmail.com
+
+>> AWS Login
+- work.sparshsharma@gmail.com
+
+>> npm i aws-sdk
+- install aws package, aws setup credentials step
+
+>> npm i multer
+- to upload something, this is req.
+
+>> cloud.google.com Login
+- work.sparshsharma@gmail.com
+
+>> npm i joi
+- this package help in validation
+
+X----------CLIENT----------X
+
+>> npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+- tailwind-css installation cmd
+
+>> https://tailwindcss.com/docs/guides/create-react-app
+- follow the steps
+
+>> npm install react-icons --save
+- react-icon npm package installer code
+
+>> npm i react-router-dom
+ Install router dom
+
+*/
